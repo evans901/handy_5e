3c95934724e6e8eac29a409dc5efebe92790095c
Ben <benevans192@gmail.com>
2021-12-11 09:10:59 -0600

    Goals

---
+++ handy_5e.py
@@ -0,0 +1,24 @@
+'''
+A handy tool to track all elements of the game Dungeons & Dragons 5th edition
+
+*** GOALS FOR THE PROJECT ***
+
+1 - Manage Charachter Sheets
+    * Long / Short Rests (resets abilites, ignore health)
+    * Classes
+    * Conditions (exhaustion, frightened, unconscious, etc.)
+
+2 - Manage Locations and NPCs
+    * Log important locations (taverns, shops, government agencys, etc.)
+    * Log important NPCs (relationships, stock items, history with party)
+
+3 - Track time in game
+    * Time within the world (Year, Month, Day)
+    * Encounter tracker
+
+*** STRETCH GOALS FOR PROJECT ***
+
+1 - Import Characters from 3rd party sites (dndbeyond, roll20, etc.)
+
+
+'''
